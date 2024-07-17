34972ffe95317a2b56f86715c973f60e7741f2ba
tonytony97 <nintego92@gmail.com>
2024-07-17 16:12:38 -0500

    Add input submission for bookDialog

--- book_construct.js
+++ book_construct.js
@@ -25,15 +25,43 @@ function addBookToLibrary() {}
 const addBookBtn = document.getElementById("add-book");
 const bookDialog = document.getElementById("dialog-book");
 const submitBtn = bookDialog.querySelector("#submit-book");
-const container = document.querySelector("#container");
+const outputBook = document.querySelector("#out-book");
+const outputAuthor = document.querySelector("#out-author");
+const outputPage = document.querySelector("#out-page");
+const outputRead = document.querySelector("#out-read");
+const inputBook = bookDialog.querySelector("#input-book");
+const inputAuthor = bookDialog.querySelector("#input-author");
+const inputPage = bookDialog.querySelector("#input-page");
+const inputRead = bookDialog.querySelector("#cb-read");
 
 addBookBtn.addEventListener("click", () => {
     bookDialog.showModal();
 });
 
-submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    bookDialog.close();
+bookDialog.addEventListener("close", (e) => {
+    outputBook.value =
+        inputBook.value === ""
+            ? "No return value."
+            : `Book Name: ${inputBook.value}.`;
+    outputAuthor.value =
+        inputAuthor.value === ""
+            ? "No return value."
+            : `Author Name: ${inputAuthor.value}.`;
+    outputPage.value =
+        inputPage.value === ""
+            ? "No return value."
+            : `Page Count: ${inputPage.value} Pages.`;
+    outputRead.value =
+        inputRead.value === ""
+            ? "No return value."
+            : `Read?: ${inputRead.checked}.`;
+    document.getElementById("form-book").reset();
+});
+
+submitBtn.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log();
+    bookDialog.close(inputBook.value, inputAuthor.value);
 });
 
 /* CreateBook test
