f283835aace234716eafdb4f40339c57aebbc13e
tonytony97 <nintego92@gmail.com>
2024-07-21 01:11:11 -0500

    Add delete button functions

--- book_construct.js
+++ book_construct.js
@@ -1,27 +1,17 @@
 const myLibrary = [];
 
 //Construct 'Book' object, capitalize first letter of constructor function
-function CreateBook(title, author, page_num, was_read) {
+function CreateBook(title, author, page_num, was_read, index_num) {
     this.title = title;
     this.author = author;
     this.page_num = page_num;
     this.was_read = was_read;
-    this.info = function () {
-        return (
-            this.title +
-            " by " +
-            this.author +
-            ", " +
-            this.page_num +
-            " pages" +
-            ", " +
-            this.was_read
-        );
-    };
+    this.index_num = index_num;
 }
-function addBookToLibrary(title, author, page_num, was_read) {
+
+function addBookToLibrary(title, author, page_num, was_read, index_num) {
     //Create new object with input values
-    const book = new CreateBook(title, author, page_num, was_read);
+    const book = new CreateBook(title, author, page_num, was_read, index_num);
     //Insert object into myLibrary array
     myLibrary.push(book);
 }
@@ -31,70 +21,69 @@ function showLibrary() {
         bookSpace = document.createElement("p");
         bookDiv1 = document.createElement("div");
         bookDiv2 = document.createElement("div");
+        bookDiv3 = document.createElement("div");
         bookTitle = document.createTextNode(`Book Name: ${i.title}`);
         bookAuthor = document.createTextNode(`Author: ${i.author}`);
         bookPages = document.createTextNode(`Pages: ${i.page_num}`);
         bookLabel = document.createElement("label");
-        bookReadText = document.createTextNode("Read?");
+        bookReadTxt = document.createTextNode("Read?");
         bookRead = document.createElement("input");
         bookRead.setAttribute("type", "checkbox");
         bookRead.checked = i.was_read;
+        delTxt = document.createTextNode("Delete Book!");
+        delBtn = document.createElement("button");
+
+        //Remove parent element from display and splice array
+        delBtn.onclick = function () {
+            n = i.index_num;
+            if (myLibrary[n + 1] === undefined) {
+                myLibrary.splice(n, 1);
+                this.parentElement.style.display = "none";
+                console.log(myLibrary);
+            } else {
+                myLibrary[n + 1].index_num -= 1;
+                myLibrary.splice(n, 1);
+                this.parentElement.style.display = "none";
+                console.log(myLibrary);
+            }
+        };
         bookSpace.appendChild(bookTitle);
         bookDiv1.appendChild(bookAuthor);
         bookSpace.appendChild(bookDiv1);
         bookDiv2.appendChild(bookPages);
         bookSpace.appendChild(bookDiv2);
-        bookLabel.appendChild(bookReadText);
+        bookLabel.appendChild(bookReadTxt);
         bookLabel.appendChild(bookRead);
-        bookSpace.appendChild(bookLabel);
+        bookDiv3.appendChild(bookLabel);
+        bookSpace.appendChild(bookDiv3);
+        delBtn.appendChild(delTxt);
+        bookSpace.appendChild(delBtn);
     }
-    bookViewer.appendChild(bookSpace);
-    console.log(bookTitle);
-    console.log(bookAuthor);
-    console.log(bookPages);
-    console.log(bookRead);
+    bookContainer.appendChild(bookSpace);
 }
 
 const addBookBtn = document.getElementById("add-book");
 const bookDialog = document.getElementById("dialog-book");
 const submitBtn = bookDialog.querySelector("#submit-book");
-const outputBook = document.querySelector("#out-book");
-const outputAuthor = document.querySelector("#out-author");
-const outputPage = document.querySelector("#out-page");
-const outputRead = document.querySelector("#out-read");
 const inputBook = bookDialog.querySelector("#input-book");
 const inputAuthor = bookDialog.querySelector("#input-author");
 const inputPage = bookDialog.querySelector("#input-page");
 const inputRead = bookDialog.querySelector("#cb-read");
-const bookViewer = document.querySelector("#book-container");
+const bookContainer = document.querySelector("#book-container");
 
 addBookBtn.addEventListener("click", () => {
     bookDialog.showModal();
 });
 
-bookDialog.addEventListener("submit", (e) => {
-    outputBook.value =
-        inputBook.value === ""
-            ? "No return value."
-            : `Book Name: ${inputBook.value}.`;
-    outputAuthor.value =
-        inputAuthor.value === ""
-            ? "No return value."
-            : `Author Name: ${inputAuthor.value}.`;
-    outputPage.value =
-        inputPage.value === ""
-            ? "No return value."
-            : `Page Count: ${inputPage.value} Pages.`;
-    outputRead.value =
-        inputRead.value === ""
-            ? "No return value."
-            : `Read?: ${inputRead.checked}.`;
+bookDialog.addEventListener("submit", () => {
     addBookToLibrary(
         inputBook.value,
         inputAuthor.value,
         inputPage.value,
         inputRead.checked,
+        myLibrary.length,
     );
+
     //Clear form inputs
     showLibrary();
     document.getElementById("form-book").reset();
@@ -112,11 +101,3 @@ submitBtn.addEventListener("submit", (event) => {
         inputRead.checked,
     );
 });
-
-/* CreateBook test
-const dune = new CreateBook("Dune", "Frank Herbert", 300, "not read yet");
-
-console.log(dune.info());
-console.log(Object.getPrototypeOf(dune) === CreateBook.prototype);
-console.log(dune.valueOf());
-*/
